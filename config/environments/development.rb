bb75ff594aa53b6eeb19eb67d3c71dda4cf1af5d
lyvanminh <lyvanminh1002@gmail.com>
2020-01-18 19:50:46 +0700

    test1

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -43,8 +43,8 @@ Rails.application.configure do
   config.action_mailer.smtp_settings = {
     address: "smtp.gmail.com",
     port: 587,
-    user_name: ENV["GMAIL_USERNAME"],
-    password: ENV["GMAIL_PASSWORD"],
+    user_name: "lyvanminh1002@gmail.com",
+    password: "minhmamanh",
     authentication: "plain",
     enable_starttls_auto: true
   }
